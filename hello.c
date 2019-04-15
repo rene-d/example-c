2393187a310f8330ff82dfae818ec18394bea502
Rene Devichi <rene.devichi@gmail.com>
2019-04-15 15:01:17 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 static int t = 1;
 
@@ -8,6 +9,21 @@ int main()
         printf("on this line\n");
     else
         printf("but not here\n");
+        
+       puts("il passera ici");
+       puts("il repassera par là");
+       
+       if (t == 0)
+       {
+       puts("mais pas ici!!!");
+       }
+       
+       for (int i = 0;i<100;++i)
+       if (rand() % 2)
+       putchar('.');
+       printf("fin\n");
+       
+       
 
     return 0;
 }
